1a7c52d9a263a061abf37050b900b3c26a7450ec
herialfaro <heriberto_alfaro_barrera@hotmail.com>
2020-07-22 18:37:40 -0500

    +Initial commit

---
+++ hola_mundo.php
@@ -0,0 +1,18 @@
+<?php
+
+//comentario
+//echo "Hola mundo";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo "Hola mundo"; ?></h1>
+</body>
+</html>
